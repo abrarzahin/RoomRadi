ebbded84488a6db9cb6a8e37edbf5bf8657ea811
abrar zahin <abrarzahin@yahoo.com>
2018-12-22 21:00:45 -0500

    1 emp

---
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/AngularSpringApplication.java
@@ -0,0 +1,14 @@
+package com.fullstack.abrarzahin.angularspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringApplication.class, args);
+	}
+
+}
+
