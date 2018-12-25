d4684e64c8539dbff1f3c613fb440f10c67ea4ab
abrar zahin <abrarzahin@yahoo.com>
2018-12-24 22:34:35 -0500

    4 post mt

--- angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/rest/ReservationResource.java
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/rest/ReservationResource.java
@@ -6,6 +6,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,7 +31,13 @@ public class ReservationResource {
 		return new ResponseEntity<>(new ReservationResponse(),HttpStatus.OK);
 		
 	}
-	
+	@RequestMapping(path= "",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+			, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> createReservation(
+			@RequestBody
+			ReservationRequest reservationRequest){
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+	}
 	
 
 }
