9f66aec6f2fb61027a3dea131de6336ca4611b16
abrar zahin <abrarzahin@yahoo.com>
2018-12-24 22:44:26 -0500

    4 R Req

--- angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/rest/ReservationResource.java
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/rest/ReservationResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fullstack.abrarzahin.angularspring.model.request.ReservationRequest;
 import com.fullstack.abrarzahin.angularspring.model.response.ReservationResponse;
 
 @RestController
