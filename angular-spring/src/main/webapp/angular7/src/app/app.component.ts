49e9154accff5a17b09aa38e6855ee833e23d703
abrar zahin <abrarzahin@yahoo.com>
2018-12-26 17:32:01 -0500

    6 avl room

--- angular-spring/src/main/webapp/angular7/src/app/app.component.ts
+++ angular-spring/src/main/webapp/angular7/src/app/app.component.ts
@@ -13,6 +13,9 @@ export class AppComponent implements OnInit {
 
   public submitted:boolean;
   roomsearch : FormGroup;
+  rooms: Room[];
+
+
   ngOnInit(){
     this.roomsearch = new FormGroup(
       {
@@ -20,12 +23,48 @@ export class AppComponent implements OnInit {
         checkout: new FormControl('')
       }
     );
+    this.rooms=ROOMS;
   }
   onSubmit({value,valid}:{value:Roomsearch,valid:boolean}){
       console.log(value);
   }
+
+  reserveRoom(value:string){
+    console.log("Room id for reservation" + value);
+  }
+
+
 }
 export interface Roomsearch{
   checkin: string;
   checkout: string;
 }
+export interface Room{
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+var ROOMS: Room[]=[
+  {
+    "id":"38932123",
+    "roomNumber":"409",
+    "price":"20",
+    "links":""
+
+  },
+  {
+    "id":"38932124",
+    "roomNumber":"4010",
+    "price":"25",
+    "links":""
+
+  },
+  {
+    "id":"38932125",
+    "roomNumber":"411",
+    "price":"30",
+    "links":""
+
+  }
+];
