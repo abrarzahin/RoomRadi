bd981d98e66c8b9ac8daeca0be6798f2c11bb84a
abrar zahin <abrarzahin@yahoo.com>
2018-12-27 23:14:23 -0500

    7 convConfig

---
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/config/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package com.fullstack.abrarzahin.angularspring.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableJpaRepositories("com.fullstack.abrarzahin.angularspring.repository")
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+}
