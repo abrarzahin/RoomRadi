f356e1758c835d20cd73f74e6a08a51316196746
abrar zahin <abrarzahin@yahoo.com>
2018-12-27 23:51:40 -0500

    7 ad jpa

---
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/H2Bootstrap.java
@@ -0,0 +1,35 @@
+package com.fullstack.abrarzahin.angularspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.fullstack.abrarzahin.angularspring.entity.RoomEntity;
+import com.fullstack.abrarzahin.angularspring.repository.RoomRepository;
+
+
+@Component
+public class H2Bootstrap implements CommandLineRunner {
+	@Autowired
+	RoomRepository roomRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Bootstrapping data:");
+		
+		roomRepository.save(new RoomEntity(405,"200"));
+		roomRepository.save(new RoomEntity(406,"220"));
+		roomRepository.save(new RoomEntity(407,"250"));
+		
+		Iterable<RoomEntity> itr= roomRepository.findAll();
+		
+		System.out.println("printing out  data:");
+		for(RoomEntity room : itr) {
+			System.out.println(room.getRoomNumber());
+		}
+		
+		
+	}
+
+}
--- angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/repository/RoomRepository.java
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/repository/RoomRepository.java
@@ -1,12 +1,13 @@
 package com.fullstack.abrarzahin.angularspring.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.fullstack.abrarzahin.angularspring.entity.RoomEntity;
 
 public interface RoomRepository extends CrudRepository<RoomEntity, Long> {
-	List<RoomEntity> findById(Long id);
+	Optional<RoomEntity> findById(Long id);
 
 }
