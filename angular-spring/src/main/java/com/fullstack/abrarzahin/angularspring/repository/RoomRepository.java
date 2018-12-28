628da9da2439936c1cee4371a903c12ca1becced
abrar zahin <abrarzahin@yahoo.com>
2018-12-27 23:34:39 -0500

    7 Room Repo

---
+++ angular-spring/src/main/java/com/fullstack/abrarzahin/angularspring/repository/RoomRepository.java
@@ -0,0 +1,12 @@
+package com.fullstack.abrarzahin.angularspring.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fullstack.abrarzahin.angularspring.entity.RoomEntity;
+
+public interface RoomRepository extends CrudRepository<RoomEntity, Long> {
+	List<RoomEntity> findById(Long id);
+
+}
